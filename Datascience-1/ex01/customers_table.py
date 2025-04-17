7758f8ad838b99ec61a53916c5604da7c38e0b7c
Ujg11 <orioljg2002@gmail.com>
2025-04-17 17:36:40 +0200

    Avanzando :)

--- Datascience-1/ex01/customers_table.py
+++ Datascience-1/ex01/customers_table.py
@@ -1,8 +1,10 @@
 import os
 import psycopg2 as psy
 import pandas as pd
+import glob
 
-CSV_FOLDER = "../customer"
+CSV_FOLDER = "../../customer"
+PATTERN = "data_202*_*.csv"
 DB_NAME = "piscineds"
 USER = "ojimenez"
 PASSWORD = "mysecretpassword"
@@ -18,16 +20,92 @@ COLUMNS = """
     user_session UUID
 """
 
-connection = psy.connect (
-    dbname = DB_NAME,
-    user = USER,
-    password = PASSWORD,
-    host = HOST,
-    port = PORT
-)
+# We create a check_columns func to chek files by pattern
+def check_columns(file_pattern):
+    csv_files = glob.glob(file_pattern)
 
-cursor = connection.cursor()
+    # We need to load the first file to have a column reference
+    reference = pd.read_csv(csv_files[0])
+    column_ref = set(reference.columns)
 
+    # We go through all the files
+    for file in csv_files[1:]:
+        df = pd.read_csv(file)
+        current_col = set(df.columns)
+        if current_col != column_ref:
+            print(f"Columns mismatch in file: {file}")
+            print(f"Expected: {column_ref}")
+            print(f"Got: {current_col}")
+            return False
+    print(f"All files starting with {file_pattern} have the same columns")
+    return True
 
+def find_csv_files(folder_path, pattern):
+    file_pattern = os.path.join(folder_path, pattern)
+    csv_files = glob.glob(file_pattern)
+    if not csv_files:
+        raise ValueError(f"No CSV files found matching {file_pattern}")
+    if check_columns(file_pattern):
+        return csv_files
+    return None
 
+def combine_csv_files(csv_files, output_file):
+    with open(output_file, 'w') as outfile:
+        # Escribimos el header
+        with open(csv_files[0], 'r') as infile:
+            header = infile.readline()
+            outfile.write(header)
 
+        for file in csv_files:
+            with open(file, 'r') as infile:
+                next(infile)
+                for line in infile:
+                    outfile.write(line)
+    return output_file
+
+def start_connection_pgadmin():
+    connection = psy.connect (
+        dbname = DB_NAME,
+        user = USER,
+        password = PASSWORD,
+        host = HOST,
+        port = PORT
+    )
+    cur = connection.cursor()
+    return connection, cur
+
+if __name__ == "__main__":
+    table_name = "customers"
+    combined_csv_file = "customers.csv"
+    
+    csv_files = find_csv_files(CSV_FOLDER, PATTERN)
+    out_file = combine_csv_files(csv_files, combined_csv_file)
+    
+    con, cur = start_connection_pgadmin()
+
+    try:
+        cur.execute(f"DROP TABLE IF EXISTS {table_name};")
+        cur.execute(f"CREATE TABLE {table_name} ({COLUMNS});")
+        print(f"Table {table_name} created")
+
+        chunk_size = 5000
+        with open(combined_csv_file, 'r') as f:
+            next(f)
+            while True:
+                # Es la funcion readline la que itera y mueve el puntero
+                lines = [f.readline() for _ in range(chunk_size)]
+                if not any(lines):
+                    break
+                cur.copy_expert(f"COPY {table_name} FROM STDIN WITH CSV", f)
+        con.commit()
+        print(f"Copied data from {combined_csv_file} into table {table_name}.")
+        
+    except Exception as e:
+        print(f"Error: {e}")
+        # Borramos los cambios hechos
+        con.rollback() 
+    finally:
+        cur.close()
+        con.close()
+
+    print("Process completed successfully.")
---
+++ Datascience-1/ex02/remove_duplicates.py
@@ -0,0 +1,62 @@
+import os
+import psycopg2 as psy
+import pandas as pd
+import glob
+
+DB_NAME = "piscineds"
+USER = "ojimenez"
+PASSWORD = "mysecretpassword"
+HOST = "localhost"
+PORT = "5432"
+
+# Con la funcion LAG creamos una columna con los datos de la fila superior llamada prev_event_time.
+# Con el primer SELECT creamos la columna extra ordenando por event_time.
+# El en segundo SELECT nos quedamos solo con las lineas que nos interesan. El EXTRACT(EPOCH FROM...) extrae el numero tootal de segundos
+# En el tercero solo borramos las filas que no queramos
+query = """
+    WITH DuplicateRows AS (
+        SELECT event_time, event_type, product_id, LAG(event_time) OVER (
+            PARTITION BY event_type, product_id
+            ORDER BY event_time
+        ) AS prev_event_time FROM customers
+    ),
+    FilteredRows AS (
+        SELECT event_time, event_type, product_id FROM DuplicateRows
+        WHERE prev_event_time IS NULL OR EXTRACT(EPOCH FROM (event_time - prev_event_time)) > 1
+    )
+    DELETE FROM customers 
+    WHERE (event_time, event_type, product_id) NOT IN (
+            SELECT event_time, event_type, product_id FROM FilteredRows
+    );
+"""
+
+def start_connection_pgadmin():
+    connection = psy.connect (
+        dbname = DB_NAME,
+        user = USER,
+        password = PASSWORD,
+        host = HOST,
+        port = PORT
+    )
+    cur = connection.cursor()
+    return connection, cur
+
+def remove_duplicates():
+
+    con, cur = start_connection_pgadmin()
+    try:
+        cur.execute(query)
+        con.commit()
+    except Exception as e:
+        print(f"Error removing: {e}")
+        con.rollback()
+    finally:
+        cur.close()
+        con.close()
+    print("Process completed successfully.")
+
+if __name__ == "__main__":
+    remove_duplicates()
+
+# before: 20687840 rows
+# after: 
